23656e9ff36c76db6180310a21bcebc5f78ecafb
jackiem2 <jacqueline.merlan@gmail.com>
2022-04-25 22:37:59 -0500

    added dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
